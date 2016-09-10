b3cd0abf6a01af0f0fb36c27fd10217f74e773e7
dulte <daniel.heinesen@sf-nett.no>
2016-09-10 09:25:01 +0200

    Add files via upload

--- solarsystem.py
+++ solarsystem.py
@@ -2,15 +2,15 @@ import numpy as np
 from numpy.linalg import norm
 import matplotlib.pyplot as plt
 from AST1100SolarSystem import AST1100SolarSystem
-
-system = AST1100SolarSystem(1595,hasMoons=False)
+seed = 69558 #daniehei5
+system = AST1100SolarSystem(seed,hasMoons=False)
 
 
 #Variables for simulations
-simulationTime = 1#In years
+simulationTime = 20#In years
 steps = 20000 #Steps per year
 dt = 1./steps
-time = 0
+
 
 times_around = 0
 
@@ -20,17 +20,15 @@ use_many_body = False
 
 #Vaaribles about the planets and star
 N = system.numberOfPlanets
-
-masses = []
-postions = np.zeros([steps*simulationTime,N + 1,2]) #Number of datapoints, numb of planets and star, 2 coordinates
-velocity = np.zeros([steps*simulationTime,N + 1,2])
-
-temp_pos = np.zeros([N + 1,2])
-temp_vel = np.zeros([N + 1,2])
+postions = np.zeros([2,N + 1,steps*simulationTime]) #Number of datapoints, numb of planets and star, 2 coordinates
+velocity = np.zeros([2,N + 1])#,steps*simulationTime])
 
 masses = np.zeros(N+1)
 periods = np.zeros(N+1)
-acceleration = np.zeros([N+1,2])
+acceleration = np.zeros([2,N+1])
+
+time = np.linspace(0,simulationTime,int(steps*simulationTime))
+print time
 
 sunMass = system.starMass
 masses[N] = sunMass
@@ -38,25 +36,27 @@ print "mass: ", sunMass
 print "Temp: ",system.temperature
 print "Radius: ",system.starRadius
 print "___"
-
+print len(system.mass)
 
 #Initializing the arrays
 for i in range(N):
     masses[i] = system.mass[i]
-    print masses[i]
-    print system.period[i]
-    print "----"
+    print "Planet %s " %str(i+1)
+    print "Planet mass: ",masses[i]
+    print "Planet radius: ",system.radius[i]
+
     postions[0,i,0] = system.x0[i]
-    postions[0,i,1] = system.y0[i]
+    postions[1,i,0] = system.y0[i]
 
 
 
-    velocity[0,i,0] = system.vx0[i]
-    velocity[0,i,1] = system.vy0[i]
+    velocity[0,i] = system.vx0[i]
+    velocity[1,i] = system.vy0[i]
 
+    periods[i] = (2*np.pi/system.period[i])*360.
+    print "Planet dtheta: ",periods[i]
 
-
-
+    print "----"
 if(use_many_body):
 
     for step in range(steps*simulationTime-1):
@@ -82,35 +82,45 @@ if(use_many_body):
         print (float(step)/(steps*simulationTime))*100, "%            \r",
     print ""
 else:
-    for step in range(steps*simulationTime-1):
-        postions[step+1,:N,:] = postions[step,:N,:] + velocity[step,:N,:]*dt + 0.5*acceleration[:N,:]*dt*dt
-        temp_vel[:N,:] = velocity[step,:N,:] + 0.5*acceleration[:N,:]*dt
-
-        for p in range(N):
-            acceleration[p,:] = -G*sunMass/(norm(postions[step+1,p,:])**3)*postions[step+1,p,:]
-        #acceleration[:N,:] = -G*sunMass/(norm(postions[step,:N,:])**3)*postions[step,:N,:]
+    print velocity[:,:N]
+    for p in xrange(N):
+        acceleration[:,p] = -G*sunMass/(norm(postions[:,p,0])**3)*postions[:,p,0]
+    velocity[:,:N] += 0.5*acceleration[:,:N]*dt
 
 
-        #velocity[step+1,:N,:] = velocity[step,:N,:] + acceleration[:N,:]*dt
-        #postions[step+1,:N,:] = postions[step,:N,:] + velocity[step+1,:N,:]*dt
-        #print postions[step+1,:,:]
+    for step in xrange(steps*simulationTime-1):
 
-        velocity[step+1,:N,:] = temp_vel[:N,:] + 0.5*acceleration[:N,:]*dt
 
-        if (np.arctan(postions[step,0,1]/postions[step,0,0]) < 0 and np.arctan(postions[step+1,0,1]/postions[step+1,0,0]) > 0 ):
+        postions[:,:N,step+1] = postions[:,:N,step] + velocity[:,:N]*dt
+        for p in xrange(N):
+            acceleration[:,p] = -G*sunMass/(norm(postions[:,p,step+1])**3)*postions[:,p,step+1]
+
+
+        velocity[:,:N] += acceleration[:,:N]*dt
+
+
+
+
+        #velocity[:,:N,step+1] = temp_vel[:,:N] + 0.5*acceleration[:,:N]*dt
+
+
+
+        if (np.arctan(postions[1,0,step]/postions[0,0,step]) < 0 and np.arctan(postions[1,0,step+1]/postions[0,0,step+1]) > 0 ):
             times_around += 1
 
         print (float(step)/(steps*simulationTime))*100, "%            \r",
     print ""
 print times_around
 
-for p in range(N-1):
-    plt.plot(postions[-1,p,0],postions[-1,p,1],"o")
-    plt.plot(postions[0,p,0],postions[0,p,1],"x")
-    plt.plot(postions[:,p,0],postions[:,p,1])
+system.checkPlanetPositions(postions,simulationTime,20000)
+#system.orbitXml(postions[:,:N,:],time)
+for p in range(N):
+    plt.plot(postions[0,p,-1],postions[1,p,-1],"o")
+    plt.plot(postions[0,p,0],postions[1,p,0],"x")
+    plt.plot(postions[0,p,:],postions[1,p,:])
 
-plt.plot(postions[-1,N,0],postions[-1,N,1],"y*")
+plt.plot(postions[0,N,-1],postions[1,N,-1],"y*")
 #plt.plot(postions[0,p,0],postions[0,p,1],"x")
-plt.plot(postions[:,N,0],postions[:,N,1])
+plt.plot(postions[0,N,:],postions[1,N,:])
 
 plt.show()
