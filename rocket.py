9678a0276343fa8d810a668598d7f1a20e205b0f
dulte <daniel.heinesen@sf-nett.no>
2016-09-19 18:07:05 +0200

    Add files via upload

--- rocket.py
+++ rocket.py
@@ -12,10 +12,10 @@ import matplotlib.pyplot as plt
 import os
 
 class engine:
-    def __init__(self, sideSize, holeSize, fuel, temp,interval,steps,number_of_particles):
+    def __init__(self, sideSize, holeSize, temp,interval,steps,number_of_particles):
         self.L = float(sideSize)
         self.hole = holeSize
-        self.fuel = fuel
+        #self.fuel = fuel
         self.T = temp
         self.k = 1.38e-23
         self.dt = interval/steps
@@ -43,6 +43,11 @@ class engine:
 
         self.v_escape = 12473
 
+        self.total_time = 0
+
+        self.fuel_list = []
+        self.velocity_list = []
+
 
 
 
@@ -56,18 +61,19 @@ class engine:
         for i in range(int(self.N)):
             for j in range(3):
                 self.x[i,j] = -self.L/2+(random.random()*self.L)
-                self.v[i,j] = random.gauss(0,self.sigma)#(3./2)*self.k*self.T)
+                self.v[i,j] = random.gauss(0,self.sigma)
 
+	
+	
 
+    def set_fuel(self,fuel):
+	self.fuel = fuel	
 
     def integrate(self):
-
-
         self.x += self.v*self.dt
 
 
     def detect_collision_vec(self):
-
 
         for i in range(3):
 
@@ -128,8 +134,8 @@ class engine:
 
     def print_press(self):
         #print "Trykk: " ,((2*self.momentum_gaines/(self.dt*self.N))/(L**2))
-        print "Trykk: " , (self.force_on_top/(self.L**2)) / steps
-        print "Utregnet trykk: ", self.N*self.k*self.T/(self.L**3)
+        print "Numerical Pressure: " , (self.force_on_top/(self.L**2))/self.steps
+        print "Analytical Pressure: ", self.N*self.k*self.T/(self.L**3)
 
     def startEngine(self):
 
@@ -145,6 +151,7 @@ class engine:
         self.mass_escaped_per_sec = self.numb_escaping*self.m/self.interval
 
         self.engine_stated = True
+        self.numb_escaping_per_sec = self.numb_escaping/self.interval
 
         print "Engine has stated!"
         print "Data from the engine: "
@@ -154,7 +161,7 @@ class engine:
         self.print_ke()
         print "Number of particles colliding with floor: ", self.numb_coll
         print "Number of particles escaped: ",self.numb_escaping
-        print "Number of particles escaped per sec: ",self.numb_escaping/self.interval
+        print "Number of particles escaped per sec: ",self.numb_escaping_per_sec
         print "Mass lost per sec: " ,self.mass_escaped_per_sec
         print "--------------------------------"
 
@@ -225,14 +232,14 @@ class engine:
 
 
 
-    def launch(self, numberOfBoxes, burnTime, launch_step):
+    def launch_simpel(self, numberOfBoxes, burnTime, launch_step):
 
         launch_dt = float(burnTime)/launch_step
 
-        total_mass_escaping_per_sec = numberOfBoxes*self.mass_escaped_per_sec
+        self.total_mass_escaping_per_sec = numberOfBoxes*self.mass_escaped_per_sec
         mass_fuel = numberOfBoxes*self.N*self.m
         time = 0
-        total_force = numberOfBoxes*self.force_gained
+        self.total_force = numberOfBoxes*self.force_gained
         fuel_mass = []
         fuel_mass.append(0)
         acceleration = 0
@@ -245,11 +252,11 @@ class engine:
 
         while (time < burnTime and launcher_vel[-1] < self.v_escape):
 
-            acceleration = total_force/(self.launcher_mass)# + mass_fuel)
+            acceleration = self.total_force/(self.launcher_mass)# + mass_fuel)
 
             launcher_vel.append(launcher_vel[-1] + acceleration*launch_dt)
 
-            fuel_mass.append(fuel_mass[-1] + total_mass_escaping_per_sec*launch_dt)
+            fuel_mass.append(fuel_mass[-1] + self.total_mass_escaping_per_sec*launch_dt)
 
             time += launch_dt
 
@@ -264,18 +271,80 @@ class engine:
         if(reached_escape_vel):
             #print "Reached escape velocity with: ", fuel[-1], " to spare!"
             print "Fuel needed: ", fuel_mass[-1]
+            print self.mass_escaped_per_sec/self.force_gained
+            print "Analytical: ", self.total_mass_escaping_per_sec*1200/(1-np.exp(self.force_gained/(self.v_escape*self.mass_escaped_per_sec))) - self.launcher_mass
             print "After ", time, " seconds!"
         else:
             print "Did not reach escape velocity"
             print "Only reach ", launcher_vel[-1], " m/s"
 
+        plt.xlabel("Timestep")
         plt.plot([self.v_escape for i in range(len(launcher_vel))])
         plt.plot(launcher_vel)
         plt.plot(fuel_mass)
-
+        plt.legend(["Escape Velocity","Velocity","Mass of fuel"], loc = 2)
         plt.show()
 
-        #return launcher_vel, fuel_mass, time, reached_escape_vel
+
+    def launch(self, numberOfBoxes, burnTime, launch_step):
+
+        launch_dt = float(burnTime)/launch_step
+
+        self.total_mass_escaping_per_sec = numberOfBoxes*self.mass_escaped_per_sec
+        time = 0
+        self.total_force = numberOfBoxes*self.force_gained
+        fuel_mass = []
+        fuel_mass.append(self.fuel)
+        acceleration = 0
+
+        self.reached_escape_vel = False
+
+
+        launcher_vel = []
+        launcher_vel.append(0)
+
+        while (time < burnTime and launcher_vel[-1] < self.v_escape):
+
+            acceleration = self.total_force/(self.launcher_mass + fuel_mass[-1])
+
+            launcher_vel.append(launcher_vel[-1] + acceleration*launch_dt)
+
+            fuel_mass.append(fuel_mass[-1] - self.total_mass_escaping_per_sec*launch_dt)
+
+            time += launch_dt
+
+            print (float(time)/burnTime)*100, "%            \r",
+        print ""
+
+        if (launcher_vel[-1] > self.v_escape):
+            self.reached_escape_vel = True
+
+        if(self.reached_escape_vel):
+            print "Reached escape velocity with: ", fuel_mass[-1], " to spare!"
+            #print "Fuel needed: ", fuel_mass[-1]
+            print "After ", time, " seconds!"
+            print "Analytical velocity: ",(self.force_gained/self.mass_escaped_per_sec)*np.log((self.launcher_mass+self.fuel)/((self.launcher_mass+self.fuel) - self.total_mass_escaping_per_sec*time))
+            print "Analytical fuel used: ", np.exp(self.mass_escaped_per_sec*self.v_escape/self.force_gained)*self.total_mass_escaping_per_sec*time/(np.exp(self.v_escape*self.mass_escaped_per_sec/self.force_gained)-1) - self.launcher_mass
+            print "Used: ", self.fuel - fuel_mass[-1]
+            self.fuel = fuel_mass[-1]
+            self.fuel_list = fuel_mass
+            self.velocity_list = launcher_vel
+            self.total_time += time
+
+        else:
+            print "Did not reach escape velocity, fell down and crashed!"
+            print "Only reach ", launcher_vel[-1], " m/s"
+            return
+
+        x = np.linspace(0,time, len(launcher_vel))
+        plt. xlabel("Time[s]")
+        plt.ylabel("Fuel[kg],Velocity[m/s]")
+        plt.plot(x,[self.v_escape for i in range(len(launcher_vel))])
+        plt.plot(x,launcher_vel)
+        plt.plot(x,fuel_mass)
+        plt.legend(["Escape Velocity","Velocity","Mass of fuel"], loc = 2)
+        plt.show()
+
 
     def checkFuelAmount(self, numberOfBoxes,fuel):
 
@@ -285,25 +354,87 @@ class engine:
 
 
 
+    def boost_fuel_check(self, boost, current_mass_of_fuel):
+        if (not self.reached_escape_vel):
+            print "It seems you havnt lauched yet"
+            return
+
+
+        dt = 1./10000
+        time = 0
+        vel = 0
+
+        while (vel < boost and current_mass_of_fuel > 0):
+            vel += (self.total_force/(self.launcher_mass+current_mass_of_fuel))*dt
+            current_mass_of_fuel -= self.total_mass_escaping_per_sec*dt
+            time += dt
+            if (current_mass_of_fuel < 0):
+                print "he"
+
+        if vel < boost:
+            print "It seem you didnt have enough fuel"
+            print "You only got ", vel, "m/s"
+        else:
+            print "You reached your desired velocity with ", current_mass_of_fuel, " fuel left"
+            print "After ", time, " sec"
+
+
+
+    def boost(self, boost):
+        if (not self.reached_escape_vel):
+            print "It seems you havnt lauched yet"
+            return
+        if (self.fuel_list[-1] == 0):
+            print "You are out of fuel, and are stranded in space..."
+
+        dt = 1./100000
+        time = 0
+
+
+        while (vel < boost and current_mass_of_fuel > 0):
+            self.velocity_list( self.velocity_list[-1] + self.total_force/(self.launcher_mass+self.fuel_list[-1]))
+            self.fuel_list.append(self.fuel_list[-1] - self.total_mass_escaping_per_sec*dt)
+            time += dt
+
+        if vel < boost:
+            print "It seem you didnt have enough fuel"
+            print "You only got ", self.velocity_list[-1], "m/s"
+        else:
+            print "You reached your desired velocity with ", self.fuel_list[-1], " fuel left"
+            print "After ", t, " sec"
+
+
+
+        self.total_time += time
+
+
+    def check_data(self):
+        print "Current velocity: ", self.velocity_list[-1], " m/s"
+        print "Current fuel: ", self.fuel_list[-1]
+        print "Time into mission: ", self.total_time
 
 
 
 
 
 
+if __name__ == "__main__":
 
-interval = 1e-9
-steps = 1000
-frames = 1000
-L = 1e-6
-H = L/2
-T = 10000
-numb_part = 1.e5#25
+	interval = 1e-9
+	steps = 1000
+	frames = 1000
+	L = 1e-6
+	H = L/2
+	T = 10000
+	numb_part = 1e5 #25
 
-e = engine(L,H,4130,T,interval,steps,numb_part)
+	e = engine(L,H,T,interval,steps,numb_part)
 
 
-#e.plot_box(frames)
+	#e.plot_box(frames)
 
-e.startEngine()
-e.launch(8.6552e12, 20*60,10000)
+	e.startEngine()
+	e.launch(1.333e13, 40*60,100000)
+	fuel = e.launcher_mass*(np.exp(e.mass_escaped_per_sec*e.v_escape/e.force_gained) - 1)#np.exp(e.mass_escaped_per_sec*e.v_escape/e.force_gained)*e.total_mass_escaping_per_sec*413/(np.exp(e.v_escape*e.mass_escaped_per_sec/e.force_gained)-1) - e.launcher_mass
+	print "Fuel needed", fuel
+	e.boost_fuel_check(e.v_escape,fuel)
---
+++ satelite.py
@@ -0,0 +1,336 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from rocket import engine
+from AST1100SolarSystem import AST1100SolarSystem
+import scipy.interpolate as inter
+from numpy.linalg import norm
+import seaborn
+
+
+class satelite:
+    def __init__(self,delta_v,destination):
+        interval = 1e-9
+        steps = 1000
+        frames = 1000
+        L = 1e-6
+        H = L/2
+        T = 10000
+        numb_part = 1e5
+
+
+
+        self.G = 4*np.pi**2
+        self.km_to_au = 6.685e-9
+
+
+
+        self.seed = 69558
+
+
+        self.e = engine(L,H,T,interval,steps,numb_part)
+        self.system = AST1100SolarSystem(self.seed)
+
+        self.force_per_box = 0
+        self.force_engine = 0
+        self.mass_lost_per_box_per_sec = 0
+        self.mass_lost_per_sec = 0
+        self.spare_delta_v = 250
+        self.fuel = 0
+        self.delta_v = delta_v + self.spare_delta_v
+        self.velocity = np.zeros(2)
+        self.position = np.zeros(2)
+        self.sim_time = 8
+        self.steps_per_year = 10000
+        self.dt = 1./self.steps_per_year
+
+        self.writeingFreq = 10
+
+        self.pos_over_time = np.zeros((2,self.sim_time*self.steps_per_year/self.writeingFreq))
+
+        self.time_planet_simulated = 20
+
+        self.use_time0 = False
+
+        self.numb_boxes = 1.333e13
+
+
+
+
+
+
+
+
+        self.numberOfPlanets = self.system.numberOfPlanets
+        self.destination_planet = destination
+        self.home_planet = 0
+
+        self.escape_velocity = self.calc_escape()
+
+
+
+        self.planetMasses = np.array(self.system.mass)
+        self.starMass = self.system.starMass
+
+
+
+        with open("positionsHomePlanet.npy", "rb") as npy:
+            self.planetPositions = np.load(npy)
+
+        # for p in range(self.numberOfPlanets):
+        #     plt.plot(self.planetPositions[0,p,-1],self.planetPositions[1,p,-1],"o")
+        #     plt.plot(self.planetPositions[0,p,0],self.planetPositions[1,p,0],"x")
+        #     plt.plot(self.planetPositions[0,p,:],self.planetPositions[1,p,:])
+        #
+        #
+        # plt.show()
+
+        self.steps =  self.planetPositions[0,0,:].size
+
+        self.time = np.linspace(0,self.time_planet_simulated,self.steps)
+
+        self.planetPosFunction = inter.interp1d(self.time, self.planetPositions)
+
+        self.calc_trad_parameters(self.destination_planet)
+        self.calc_time_to_burn(self.angle_between_planets)
+        self.launch_angle = self.calc_tangental_angle(self.time_to_launch)
+
+
+        if (self.use_time0):
+            self.time_to_launch = 0
+
+        print "e home: ", self.system.e[0]
+        print "e destinaton: ", self.system.e[self.destination_planet]
+
+
+        #v0 = [self.system.vx0[self.home_planet] + -np.cos(self.launch_angle)*(1.*self.calc_influence() +.956*self.dv_mainburn),0.*self.system.vy0[self.home_planet] + np.sin(self.launch_angle)*(1.*self.calc_influence() +.956*self.dv_mainburn)]#[0. + .2*self.escape_velocity,0*self.system.vy0[self.home_planet] + 0.3*self.escape_velocity]
+        """0*0.767"""
+        # self.position = np.array([self.system.x0[0],self.system.y0[0]])
+        # v0 = [0,self.dv_mainburn]
+
+        print "Angle: ",self.calc_tangental_angle(self.time_to_launch)
+        print "Sin, ",np.sin(self.calc_tangental_angle(self.time_to_launch))
+        print "Cos: ",np.cos(self.calc_tangental_angle(self.time_to_launch))
+
+        self.position = np.array([self.planetPosFunction(self.time_to_launch)[0,0],self.planetPosFunction(self.time_to_launch)[1,0]])
+        v0 = [-np.sin(self.calc_tangental_angle(self.time_to_launch))*(self.dv_mainburn),np.cos(self.calc_tangental_angle(self.time_to_launch))*(self.dv_mainburn)]
+        print v0
+
+        # if (self.use_time0):
+        #     v0 = [self.system.vx0[self.home_planet] ,0.*self.system.vy0[self.home_planet] + 0.*self.calc_influence() +1*self.dv_mainburn] #.956*self.dv_mainburn]#[0. + .2*self.escape_velocity,0*self.system.vy0[self.home_planet] + 0.3*self.escape_velocity]
+        #     self.position = np.array([self.system.x0[0] +0*self.system.radius[self.home_planet]*self.km_to_au ,self.system.y0[0] + 1*self.system.radius[self.home_planet]*self.km_to_au])
+        # else:
+        #     v0 = [0*self.calc_vel(self.home_planet, self.time_to_launch)[0]+ -np.sin(self.launch_angle)*(0.*self.calc_influence() +1.048*self.dv_mainburn),0.*self.system.vy0[self.home_planet] + np.cos(self.launch_angle)*(0.*self.calc_influence() +1.048*self.dv_mainburn)]#[0. + .2*self.escape_velocity,0*self.system.vy0[self.home_planet] + 0.3*self.escape_velocity]
+        #     self.position = self.planetPosFunction(self.time_to_launch)[:,0] + np.array([-np.sin(self.launch_angle)*(self.system.radius[self.home_planet]*self.km_to_au),np.cos(self.launch_angle)*(self.system.radius[self.home_planet]*self.km_to_au)])
+        #
+        #print norm(np.array(v0))
+
+
+
+
+        self.velocity = np.array(v0)
+        self.pos_over_time[:,0] = self.position
+
+
+
+
+
+    def main_sequence(self):
+
+        self.e.startEngine()
+        self.force_per_box = self.e.force_gained
+        self.mass_lost_per_box_per_sec = self.e.mass_escaped_per_sec
+        self.force_engine = self.force_per_box*self.numb_boxes
+        self.mass_lost_per_sec = self.mass_lost_per_box_per_sec*self.numb_boxes
+        print "Engine ignited"
+        self.fuel_cal(self.delta_v)
+        print "Lauching with ", self.fuel, " kg fuel."
+        print "---------------------"
+
+
+    def fuel_cal(self,delta_v):
+        self.fuel = self.e.launcher_mass*(np.exp(self.e.mass_escaped_per_sec*delta_v/self.e.force_gained) - 1)
+
+
+    def boost(self,delta_v):
+        pass
+
+
+    def acceleration(self,planetPos):
+        # acceleration = np.zeros(2)
+        # r = planetPos.T - self.position
+        #
+        # to_low = np.abs(r) < self.system.radius[self.home_planet]*self.km_to_au
+        # #r[to_low] = self.system.radius[self.home_planet]*self.km_to_au
+        #
+        # acceleration = np.sum((self.G*self.planetMasses)/(norm(r,axis = 1)**3)*r.T,axis = 1) #-(self.G*self.starMass/(norm(self.position,axis = 0)**3))*self.position
+        # return acceleration
+        # return -(self.G*self.starMass/(norm(self.position,axis = 0)**3))*self.position
+
+
+        acceleration = np.zeros(2)
+
+        rx = (planetPos[0,:] - self.position[0])
+        ry = (planetPos[1,:] - self.position[1])
+
+        some = norm(np.array([rx,ry]),axis = 0)
+        acceleration[0] = np.sum((self.G*self.planetMasses[:]/(some**3)*rx))
+        acceleration[1] = np.sum((self.G*self.planetMasses[:]/(some**3)*ry))
+        #return acceleration -(self.G*self.starMass/(norm(self.position,axis = 0)**3))*self.position
+        return -(self.G*self.starMass/(norm(self.position,axis = 0)**3))*self.position
+
+
+    def main_loop(self):
+
+
+        print "---------------------------------"
+        print "Beginning to calculate the orbit: "
+
+        if (self.use_time0):
+            time = [0.]
+        else:
+            time =[self.time_to_launch]
+
+        time_save = []
+
+        self.velocity += 0.5*self.acceleration(self.planetPosFunction(time[-1]))*self.dt
+        #print self.planetPosFunction(time[-1])
+
+        min_dist_to_planet = 1000
+        time_closest_incounter = 0
+
+        dt = self.dt
+
+        for i in xrange(1,self.steps_per_year*self.sim_time-1):
+
+
+
+            self.position += self.velocity*dt
+
+            time.append(time[-1] + dt)
+            self.velocity += self.acceleration(self.planetPosFunction(time[-1]))*dt
+            #self.velocity += self.acceleration(self.planetPosFunction(0))*self.dt
+
+            if ((i+1)%self.writeingFreq == 0 ):
+                self.pos_over_time[:,i/self.writeingFreq+1] = self.position
+                time_save.append(time[-1])
+
+            if (abs(self.calc_dist_to_planet(self.destination_planet,time[-1],self.position)) < min_dist_to_planet):
+                min_dist_to_planet = abs(self.calc_dist_to_planet(self.destination_planet,time[-1],self.position))
+                time_closest_incounter = time[-1]
+
+            print (float(i)/(self.steps_per_year*self.sim_time))*100, "%            \r",
+        print ""
+
+
+
+        print "Shortest distance to planet: ", min_dist_to_planet
+        print "At time: ",time_closest_incounter
+        plt.plot(self.pos_over_time[0,0],self.pos_over_time[1,0],"x")
+        plt.plot(self.pos_over_time[0,:],self.pos_over_time[1,:])
+        plt.plot(self.pos_over_time[0,-1],self.pos_over_time[1,-1],"o")
+
+        #plt.plot(self.planetPosFunction(self.))
+
+        pos_planets =  self.planetPosFunction(np.array(time_save))
+
+        for p in xrange(self.numberOfPlanets):
+            plt.plot(pos_planets[0,p,:],pos_planets[1,p,:])
+
+
+        plt.plot(self.planetPosFunction(time_closest_incounter)[0,self.destination_planet],self.planetPosFunction(time_closest_incounter)[1,self.destination_planet],"*")
+
+
+
+
+
+        plt.axis("equal")
+        plt.show()
+
+
+    def calc_escape(self):
+
+        return np.sqrt(2*self.G*self.system.mass[self.home_planet]/(self.system.radius[self.home_planet]*self.km_to_au))
+
+    def calc_influence(self):
+        k = 10
+        r = norm(np.array([self.system.x0[0],self.system.y0[0]]))
+        r_soi = r*(self.planetMasses[0]/self.starMass)**(2./5)
+
+        r_inf = r/np.sqrt(k*self.starMass/self.planetMasses[0])
+        return np.sqrt(-(2*self.G*self.planetMasses[0])/(r_soi) + 2*self.G*self.planetMasses[0]/(self.system.radius[0]*self.km_to_au))
+
+    def calc_trad_parameters(self,planet_number):
+
+        time_mod = -0.
+
+        r1 = norm(np.array([self.system.x0[0],self.system.y0[0]]))
+        r2 = norm(np.array([self.system.x0[planet_number],self.system.y0[planet_number]]))
+
+
+        self.dv_mainburn = np.sqrt(2*(self.G*self.starMass/r1 - self.G*self.starMass/(r1+r2)))
+
+        self.time_to_encounter = time_mod + np.pi*np.sqrt((r1+r2)**3/(8*self.G*self.starMass))
+
+        self.omega_destination = 2*np.pi/(np.sqrt((4*np.pi**2 * self.system.a[planet_number]**3)/(self.G*(self.starMass+self.planetMasses[planet_number]))))
+        self.omega_home = 2*np.pi/(np.sqrt((4*np.pi**2 * self.system.a[self.home_planet]**3)/(self.G*(self.starMass+self.planetMasses[self.home_planet]))))
+        self.angle_between_planets = np.pi - self.omega_destination*self.time_to_encounter
+
+        print "Burn required: ",self.dv_mainburn
+        print "Trip takes: ", self.time_to_encounter
+        print "Planet moves at angular v: ", self.omega_destination
+        print "Home planets anguar v: ", self.omega_home
+        print "Angle between planet when burn: ",self.angle_between_planets
+
+    def calc_time_to_burn(self,angle):
+
+        print "Beginning to calculate burn time. This may take a couple of minutes..."
+
+        min_angle = 10000.
+        time_for_min_angle = 0
+        #time = 0.0
+        time = 7.
+        eps = 1.e-4
+
+        sim_time = 20.
+        steps = 20000
+        dt = 1./steps
+        for i in xrange(1,int(steps*sim_time)-1):
+            angle_home = np.arctan2(self.planetPosFunction(time)[1,self.home_planet],self.planetPosFunction(time)[0,self.home_planet])
+            angle_destination = np.arctan2(self.planetPosFunction(time)[1,self.destination_planet],self.planetPosFunction(time)[0,self.destination_planet])
+
+            time += dt
+
+            if abs((angle_destination - angle_home)-angle) < eps:
+                break
+
+            print (float(i)/(steps*sim_time))*100, "%            \r",
+        print ""
+
+
+        self.time_to_launch = time
+        print "Smallest difference in angle: ",abs((angle_destination - angle_home)-angle)
+        print "At time: ",self.time_to_launch
+
+
+    def calc_dist_to_planet(self,planet_number,time,pos):
+
+        r = norm(self.planetPosFunction(time)[:,planet_number] - pos)
+        return abs(r)
+
+    def calc_tangental_angle(self,time):
+        return  np.arctan2(self.planetPosFunction(time)[1,self.home_planet],self.planetPosFunction(time)[0,self.home_planet])
+
+
+    def calc_vel(self,planet_number,time):
+        dx = (self.planetPosFunction(time+self.dt)[0,planet_number] - self.planetPosFunction(time-self.dt)[0,planet_number])/(2*self.dt)
+        dy = (self.planetPosFunction(time+self.dt)[1,planet_number] - self.planetPosFunction(time-self.dt)[1,planet_number])/(2*self.dt)
+
+        return np.array([dx,dy])
+
+
+
+destinaton_planet = 1
+sat = satelite(20000,destinaton_planet)
+#sat.main_sequence()
+print sat.calc_escape()
+sat.main_loop()
